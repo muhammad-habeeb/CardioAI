7204b853229f272f8323fdb45f7797a8ccfcad51
Muhammad Habeeb <muhammadhabeeb.cs@gmail.com>
2026-08-06 00:51:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import streamlit as st
 # ------------------------------------------------------------------
 st.set_page_config(
     page_title="Heart Attack Risk Predictor",
-    page_icon="❤️",
+    page_icon="🫀",
     layout="wide",
     initial_sidebar_state="expanded",
 )
